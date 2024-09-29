28d0ec25422568f350fd3777bcb1d0a47a75c780
davidlukashevich <david.lukashevich7@gmail.com>
2024-09-29 20:49:38 +0200

    Change API address

--- weather-app/src/redux/api/api.js
+++ weather-app/src/redux/api/api.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export const WeatherAPI = {
     getCityInfo(city) {
-        return axios.get(`http://api.weatherstack.com/forecast?access_key=897c70b25af47ffbc836d7b9dadd3e4d&query=${city}`);
+        return axios.get(`https://api.weatherstack.com/forecast?access_key=897c70b25af47ffbc836d7b9dadd3e4d&query=${city}`);
     }
 }
